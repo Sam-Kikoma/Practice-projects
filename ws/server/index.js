ceb1e05c607be6d4c0787a91567bbaa4d3f9acc8
Sam Kikoma <kikomasamson@gmail.com>
2025-11-23 15:25:20 +0100

    updates

---
+++ ws/server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const server = createServer(app);
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST"],
+	},
+});
+io.on("connection", (socket) => {
+	console.log(`User is connected : ${socket.id}`);
+	socket.on("send_message", (data) => {
+		socket.broadcast.emit("receive_message", data);
+	});
+});
+
+server.listen(3001, () => {
+	console.log("Server is live");
+});
